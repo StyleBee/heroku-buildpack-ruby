37c2dd11147440d05f861bee40fec5b7ca7fbbc8
Kyle Wilkinson <kai@wikyd.org>
2013-05-17 19:53:37 -0700

    Try loading libraries.

--- lib/binary_dependencies.rb
+++ lib/binary_dependencies.rb
@@ -12,13 +12,20 @@ module BinaryDependencies
   # @return +nil+
   def self.vendor(*args)
     ::Dir.chdir(args.first)
+    lib_so_conf_dir = "/etc/ld.so.conf.d"
+    ::FileUtils.mkdir_p(lib_so_conf_dir)
+
     DEPENDENCIES.each do |name, url|
       bin_dir = "vendor/#{name}"
       ::FileUtils.mkdir_p bin_dir
 
+
       puts "Downloading #{name} from #{url}"
       ::Dir.chdir(bin_dir) do |dir|
         run("curl #{url} -s -o - | tar xzf -")
+        ::File.open("#{lib_so_conf_dir}/#{name}.conf", 'w') do |file|
+          file.write("/app/bin/#{name}/lib")
+        end
       end
     end
   end
